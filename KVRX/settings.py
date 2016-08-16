a837f42a8cc6943599b2f4f390e6dbfb0517ffd8
Jorge Hernandez <jm.hernandez@utexas.edu>
2016-08-15 19:03:43 -0500

    Changed the time format from 24hr to 12hr. I also changed the time zone to central time (UTC-6:00).

--- KVRX/settings.py
+++ KVRX/settings.py
@@ -106,7 +106,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Chicago'
+TIME_INPUT_FORMATS = ['%I:%M']
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
